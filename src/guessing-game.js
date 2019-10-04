08ec379351a5e28074ed0f8dcca66314663444a3
EldarBildanov <eldar.bildanov@gmail.com>
2019-10-04 21:01:07 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.L = null
+        this.R = null 
+    }
 
     setRange(min, max) {
-
+        this.L = min
+        this.R = max
     }
 
     guess() {
-
+        return Math.round((this.R + this.L)/2)
     }
 
     lower() {
-
+        this.setRange(this.L, Math.round((this.R + this.L)/2))
     }
 
     greater() {
+        this.setRange(Math.round((this.R + this.L)/2), this.R)
 
     }
 }
